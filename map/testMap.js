1f2650ca2637db80f1a2b6fe4f5582f65e2b364e
kojikoji <example@example.com>
2016-11-26 23:22:04 +0900

    自販機・ガソリンスタンド表示

--- map/testMap.js
+++ map/testMap.js
@@ -3,13 +3,14 @@ var map;
 function initMap(){
 
     var mapId ={
-	zoom: 13,
+	zoom: 12,
 	center: new google.maps.LatLng(35.890358,136.344221)
     };
 
     map = new google.maps.Map(document.getElementById("map"),mapId);
 }
 
+/*公共クラウドの全国データ*/
 function setMaker(name,lat, lng, info, link){
 
     var latlng = new google.maps.LatLng(lat,lng);
@@ -18,11 +19,6 @@ function setMaker(name,lat, lng, info, link){
 	map: map
     });
 
-    
-/*    var infoWindow = new google.maps.InfoWindow({
-	content: '<div class="marker"><a href=' + img + ' target=_blank><img width=100% src=' + img + '></a>'+info+'</div>'
-    });
-*/
     var s = name; 
     if(link){
 	s = '<a traget=_blank href='+link+'>'+name+'</a>';
@@ -44,7 +40,8 @@ function setMaker(name,lat, lng, info, link){
 	infoWindow.close();
     });
 }
- 
+
+/*自販機マーカー用*/
 function setMakerVM(location,lat, lng){
 
     var latlng = new google.maps.LatLng(lat,lng);
@@ -54,12 +51,65 @@ function setMakerVM(location,lat, lng){
     });
 
     var s = location; 
-    if(s){
 
-   var infoWindow = new google.maps.InfoWindow({
+    var infoWindow = new google.maps.InfoWindow({
 	content: s
-             
+     });
+    
+    google.maps.event.addListener(marker,'mouseover',function(){
+	infoWindow.open(map, marker);
     });
 
-    }
+    google.maps.event.addListener(marker,'mouseout',function(){
+	infoWindow.close();
+    });
+
+}
+/*ガソリンスタンドマーカー用*/
+function setMarkerSS(label,lat, lng){
+
+    var latlng = new google.maps.LatLng(lat,lng);
+    var marker = new google.maps.Marker({
+	position: latlng,
+	map: map
+    });
+
+    var s = label; 
+
+    var infoWindow = new google.maps.InfoWindow({
+	content: s
+     });
+    
+    google.maps.event.addListener(marker,'mouseover',function(){
+	infoWindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker,'mouseout',function(){
+	infoWindow.close();
+    });
+
+}
+/*バス停マーカー用*/
+function setMarkerBR(label,lat, lng){
+
+    var latlng = new google.maps.LatLng(lat,lng);
+    var marker = new google.maps.Marker({
+	position: latlng,
+	map: map
+    });
+
+    var s = label; 
+
+    var infoWindow = new google.maps.InfoWindow({
+	content: s
+     });
+    
+    google.maps.event.addListener(marker,'mouseover',function(){
+	infoWindow.open(map, marker);
+    });
+
+    google.maps.event.addListener(marker,'mouseout',function(){
+	infoWindow.close();
+    });
+
 }
